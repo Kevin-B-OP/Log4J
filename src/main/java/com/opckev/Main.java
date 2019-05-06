b6b6a386e30bd6ce277de25f1da91453d64bef61
kevin <kevin.bony@protonmail.com>
2019-05-06 17:04:04 +0200

    Initial commit

---
+++ src/main/java/com/opckev/Main.java
@@ -0,0 +1,19 @@
+package com.opckev;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+
+    private static final Logger LOG = LogManager.getLogger(Main.class);
+
+    public static void main(String... args){
+        LOG.debug("This will be printed on debug");
+        LOG.info("This will be printed on info");
+        LOG.warn("This will be printed on warn");
+        LOG.error("This will be printed on error");
+        LOG.fatal("This will be printed on fatal");
+
+        LOG.info("Appending string: {}.", "Hello, World");
+    }
+}
